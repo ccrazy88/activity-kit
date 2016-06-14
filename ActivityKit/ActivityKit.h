a6fd86eafda442f7ea4915a9213d9ae5fe0fc492
Chrisna Aing <chrisna@chrisna.org>
2016-06-14 01:05:12 -0700

    Initial Commit

---
+++ ActivityKit/ActivityKit.h
@@ -0,0 +1,19 @@
+//
+//  ActivityKit.h
+//  ActivityKit
+//
+//  Created by Chrisna Aing on 6/14/16.
+//  Copyright © 2016 Chrisna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ActivityKit.
+FOUNDATION_EXPORT double ActivityKitVersionNumber;
+
+//! Project version string for ActivityKit.
+FOUNDATION_EXPORT const unsigned char ActivityKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ActivityKit/PublicHeader.h>
+
+
